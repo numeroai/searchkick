3edab5d106ef6a8ec3e4ac53f0a2b3ba47d9943e
Rosie Ingmann <rosie@numero.ai>
2026-04-30 14:40:02 -0400

    cleanup

--- lib/searchkick/indexer.rb
+++ lib/searchkick/indexer.rb
@@ -13,27 +13,6 @@ module Searchkick
       perform unless Searchkick.callbacks_value == :bulk
     end
 
-    # def perform
-    #   items = @queued_items
-    #   @queued_items = []
-
-    #   return if items.empty?
-
-    #   response = Searchkick.client.bulk(body: items)
-    #   if response["errors"]
-    #     # note: delete does not set error when item not found
-    #     first_with_error = response["items"].map do |item|
-    #       (item["index"] || item["delete"] || item["update"])
-    #     end.find.with_index { |item, i| item["error"] && !ignore_missing?(items[i], item["error"]) }
-    #     if first_with_error
-    #       raise ImportError, "#{first_with_error["error"]} on item with id '#{first_with_error["_id"]}'"
-    #     end
-    #   end
-
-    #   # maybe return response in future
-    #   nil
-    # end
-    # 
     def perform
       items = @queued_items
       @queued_items = []
@@ -75,11 +54,5 @@ module Searchkick
 
       nil
     end
-
-    private
-
-    # def ignore_missing?(item, error)
-    #   error["type"] == "document_missing_exception" && item.instance_variable_defined?(:@ignore_missing)
-    # end
   end
 end
